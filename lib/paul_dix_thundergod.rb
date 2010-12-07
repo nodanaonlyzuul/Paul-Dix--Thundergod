3b7ad02ee2e44a024d07ce14eb21aacd246f443f
Mike Dalessio <mike@csa.net>
2010-12-07 13:18:43 -0500

    added support for 'play' (for linux peeps)

--- lib/paul_dix_thundergod.rb
+++ lib/paul_dix_thundergod.rb
@@ -1,5 +1,12 @@
 module PaulDixThundergod
+  COMMANDS_TO_TRY = %w[afplay play]
+
   def self.play
-    `afplay #{File.dirname(__FILE__)}/paul_dix_thundergod/support/deploy_sound.aiff &`
+    COMMANDS_TO_TRY.each do |command|
+      if system("which #{command}")
+        `#{command} #{File.dirname(__FILE__)}/paul_dix_thundergod/support/deploy_sound.aiff &`
+        break
+      end
+    end
   end
 end
