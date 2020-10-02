6e913388bd28e537b27829dd9a9e37130dc7c3d1
Mike Dalessio <mike.dalessio@gmail.com>
2020-10-02 16:55:20 -0400

    thundergod is now up in your desktop notifications
    like a boss

--- lib/paul_dix_thundergod.rb
+++ lib/paul_dix_thundergod.rb
@@ -2,6 +2,10 @@ module PaulDixThundergod
   COMMANDS_TO_TRY = %w[afplay play omxplayer]
 
   def self.play
+    if system("which notify-send")
+      `notify-send -i #{asset_path("thundergod.png")} "DEPLOY" "Good luck and godspeed."`
+    end
+
     COMMANDS_TO_TRY.each do |command|
       if system("which #{command}")
         `#{command} #{asset_path("deploy_sound.mp3")} &`
